3408489f2f84c2b5d80ba3f363648fbd4eaec951
thakun399 <thakun.niam@bumail.net>
2025-04-28 21:25:18 +0700

    5

--- Assets/สคลิป/โปรเจ็กทายT2.cs
+++ Assets/สคลิป/โปรเจ็กทายT2.cs
@@ -1,16 +1,41 @@
+using System;
+using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class โปรเจ็กทายT2 : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    [SerializeField] Transform shootpoint;
+    [SerializeField] GameObject target;
+    [SerializeField] Rigidbody2D bulletprefab;
+
+    private void Update()
     {
-        
+        if (Input.GetMouseButtonDown(0))
+        {
+            Ray Ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            Debug.DrawRay(Ray . origin, Ray.direction * 5f, Color.yellow , 5f);
+            RaycastHit2D hit = Physics2D.GetRayIntersection(Ray,Mathf.Infinity);
+
+            if (hit.collider != null)
+            {
+                target . transform.position = new Vector2(hit.point.x , hit.point.y);
+                Debug.Log("hit" + hit.collider.name);
+                Vector2 ProjectileVelocity = CalculateProjectileVelocity(shootpoint.position, hit.point,1f);
+                Rigidbody2D shootBullrt = Instantiate(bulletprefab,shootpoint.position,Quaternion.identity,shootpoint);
+                shootBullrt.linearVelocity = ProjectileVelocity;
+                
+            }
+        }
     }
 
-    // Update is called once per frame
-    void Update()
+    Vector2 CalculateProjectileVelocity(Vector2 origin, Vector2 target, float time)
     {
+        Vector2 distance = target - origin;
+        float velocitX = distance.x / time;
+        float velocityY = distance.y / time + 0.5f * Mathf.Abs(Physics2D.gravity.y);
         
+        Vector2 projectileVelocity = new Vector2(velocitX , velocityY);
+        return projectileVelocity;
     }
 }
